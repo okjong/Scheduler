a05a9d96a57aa381e3fc41c8301254e7b37b322e
okjong <ljo4954@gmail.com>
2022-11-04 16:42:36 +0900

    manifest 추가

--- app/src/main/java/com/jeilpharm/project11/ListFragmnet.java
+++ app/src/main/java/com/jeilpharm/project11/ListFragmnet.java
@@ -59,10 +59,14 @@ public class ListFragmnet extends Fragment {
 
                         switch (menuItem.getItemId()){
                             case R.id.popup_schedule:
+                                Intent intent= new Intent(getActivity(),ScheduleActivity.class);
                                 break;
                             case R.id.popup_graph:
+                                Intent intent1= new Intent(getActivity(),GraphActivity.class);
                                 break;
                             case R.id.popup_location:
+                                Intent intent2= new Intent(getActivity(),MapActivity2.class);
+                                break;
                         }
 
 
