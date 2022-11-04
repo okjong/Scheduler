1652a2c304d57859fda1525e9868ea4f84afd83b
okjong <ljo4954@gmail.com>
2022-11-04 14:13:53 +0900

    manifest 추가

--- app/src/main/java/com/jeilpharm/project11/AddActivity.java
+++ app/src/main/java/com/jeilpharm/project11/AddActivity.java
@@ -1,24 +1,14 @@
 package com.jeilpharm.project11;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.jeilpharm.project11.databinding.ActivityAddBinding;
 
 public class AddActivity extends AppCompatActivity {
 
     ActivityAddBinding binding;
-
-    int category;
-    String[] categorytitle= new String[]{"웹심포지움","저녁세미나","숙박심포지움","점심식사","저녁식사","조조판촉","간식"};
-
-    String date="2022년11월3일(수) 22:00PM";
-
-    BottomSheetDialog bottomSheetDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
