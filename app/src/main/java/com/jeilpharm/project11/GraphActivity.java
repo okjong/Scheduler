26f996fc1467f0be262d2d333ddd2a2327accf3b
okjong <ljo4954@gmail.com>
2022-11-05 17:16:26 +0900

    third commit

--- app/src/main/java/com/jeilpharm/project11/GraphActivity.java
+++ app/src/main/java/com/jeilpharm/project11/GraphActivity.java
@@ -10,5 +10,8 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+
+
     }
 }
