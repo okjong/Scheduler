1c0be8167d50e4881b9754b8b9936bd4e5fea1d2
okjong <ljo4954@gmail.com>
2022-11-10 16:38:40 +0900

    manifest 추가

--- app/src/main/java/com/jeilpharm/project11/MonthFragment.java
+++ app/src/main/java/com/jeilpharm/project11/MonthFragment.java
@@ -1,5 +1,6 @@
 package com.jeilpharm.project11;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -20,8 +27,110 @@ import java.io.IOException;
 
 public class MonthFragment extends Fragment {
 
+    MaterialCalendarView materialCalendarView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_month,container,false);
 
 
+        materialCalendarView=view.findViewById(R.id.calendarView);
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(MaterialCalendarView widget, CalendarDay date, boolean selected) {
 
 
+            }
+        });
+
+
+        return view;
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/jeilpharm/project11/TodoActivity.java
+++ app/src/main/java/com/jeilpharm/project11/TodoActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 
 import com.jeilpharm.project11.databinding.ActivityTodoBinding;
 
+import java.util.ArrayList;
+
 public class TodoActivity extends AppCompatActivity {
 
     ActivityTodoBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityTodoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
 
 
