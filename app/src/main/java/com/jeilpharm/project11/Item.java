465d618540310e31cbc47d84a45a0d7445456069
okjong <ljo4954@gmail.com>
2022-11-11 15:21:43 +0900

    manifest 추가

---
+++ app/src/main/java/com/jeilpharm/project11/Item.java
@@ -0,0 +1,17 @@
+package com.jeilpharm.project11;
+
+public class Item {
+
+    String hospital;
+    String title;
+    String date;
+
+    public Item(String hospital, String title, String date) {
+        this.hospital = hospital;
+        this.title = title;
+        this.date = date;
+    }
+
+    public Item() {
+    }
+}
--- app/src/main/java/com/jeilpharm/project11/TodoRecyclerAdapter.java
+++ app/src/main/java/com/jeilpharm/project11/TodoRecyclerAdapter.java
@@ -1,4 +1,68 @@
 package com.jeilpharm.project11;
 
-public class TodoRecyclerAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class TodoRecyclerAdapter extends RecyclerView.Adapter<TodoRecyclerAdapter.VH> {
+
+    Context context;
+    ArrayList<Item> items;
+
+    public TodoRecyclerAdapter(Context context, ArrayList<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView= LayoutInflater.from(context).inflate(R.layout.recyclerview_item,parent,false);
+
+        return new VH(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH holder, int position) {
+
+        Item item= items.get(position);
+
+        holder.rcv_tv_hospital.setText(item.hospital);
+        holder.rcv_tv_title.setText(item.title);
+        holder.rcv_tv_date.setData(item.date);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class VH extends RecyclerView.ViewHolder{
+
+        TextView rcv_tv_hospital;
+        TextView rcv_tv_title;
+        Text rcv_tv_date;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+
+            rcv_tv_hospital=itemView.findViewById(R.id.recyclerview_item_tv_hospital);
+            rcv_tv_title=itemView.findViewById(R.id.recyclerview_item_tv_title);
+            rcv_tv_date=itemView.findViewById(R.id.recyclerview_item_tv_date);
+
+        }
+    }
+
+
 }
